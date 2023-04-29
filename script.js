f6cb8fc0e06beb57b990c2f1e4dd62910086020c
Martynjuk Ivan <martiniukgomel@gmail.com>
2023-04-29 20:49:03 +0300

    feat: add shift & caps func 29 апр 2023 г. 20:49:03

--- script.js
+++ script.js
@@ -93,75 +93,203 @@ body.insertAdjacentHTML('afterbegin', `<header class="header">
 // Add values
 
 const keysArr = [
-  { id: 'Backquote', valueEn: '`', valueRu: 'ё' },
-  { id: 'Digit1', valueEn: '1', valueRu: '1' },
-  { id: 'Digit2', valueEn: '2', valueRu: '2' },
-  { id: 'Digit3', valueEn: '3', valueRu: '3' },
-  { id: 'Digit4', valueEn: '4', valueRu: '4' },
-  { id: 'Digit5', valueEn: '5', valueRu: '5' },
-  { id: 'Digit6', valueEn: '6', valueRu: '6' },
-  { id: 'Digit7', valueEn: '7', valueRu: '7' },
-  { id: 'Digit8', valueEn: '8', valueRu: '8' },
-  { id: 'Digit9', valueEn: '9', valueRu: '9' },
-  { id: 'Digit0', valueEn: '0', valueRu: '0' },
-  { id: 'Minus', valueEn: '-', valueRu: '-' },
-  { id: 'Equal', valueEn: '=', valueRu: '=' },
-  { id: 'Backspace', valueEn: 'BackSpace', valueRu: 'BackSpace' },
-  { id: 'Tab', valueEn: 'Tab', valueRu: 'Tab' },
-  { id: 'KeyQ', valueEn: 'q', valueRu: 'й' },
-  { id: 'KeyW', valueEn: 'w', valueRu: 'ц' },
-  { id: 'KeyE', valueEn: 'e', valueRu: 'у' },
-  { id: 'KeyR', valueEn: 'r', valueRu: 'к' },
-  { id: 'KeyT', valueEn: 't', valueRu: 'е' },
-  { id: 'KeyY', valueEn: 'y', valueRu: 'н' },
-  { id: 'KeyU', valueEn: 'u', valueRu: 'г' },
-  { id: 'KeyI', valueEn: 'i', valueRu: 'ш' },
-  { id: 'KeyO', valueEn: 'o', valueRu: 'щ' },
-  { id: 'KeyP', valueEn: 'p', valueRu: 'з' },
-  { id: 'BracketLeft', valueEn: '[', valueRu: 'х' },
-  { id: 'BracketRight', valueEn: ']', valueRu: 'ъ' },
-  { id: 'Backslash', valueEn: '\\', valueRu: '\\' },
-  { id: 'CapsLock', valueEn: 'Caps', valueRu: 'Caps' },
-  { id: 'KeyA', valueEn: 'a', valueRu: 'ф' },
-  { id: 'KeyS', valueEn: 's', valueRu: 'ы' },
-  { id: 'KeyD', valueEn: 'd', valueRu: 'в' },
-  { id: 'KeyF', valueEn: 'f', valueRu: 'а' },
-  { id: 'KeyG', valueEn: 'g', valueRu: 'п' },
-  { id: 'KeyH', valueEn: 'h', valueRu: 'р' },
-  { id: 'KeyJ', valueEn: 'j', valueRu: 'о' },
-  { id: 'KeyK', valueEn: 'k', valueRu: 'л' },
-  { id: 'KeyL', valueEn: 'l', valueRu: 'д' },
-  { id: 'Semicolon', valueEn: ';', valueRu: 'ж' },
-  { id: 'Quote', valueEn: '\'', valueRu: 'э' },
-  { id: 'Enter', valueEn: 'Enter', valueRu: 'Enter' },
-  { id: 'ShiftLeft', valueEn: 'Shift', valueRu: 'Shift' },
-  { id: 'KeyZ', valueEn: 'z', valueRu: 'я' },
-  { id: 'KeyX', valueEn: 'x', valueRu: 'ч' },
-  { id: 'KeyC', valueEn: 'c', valueRu: 'с' },
-  { id: 'KeyV', valueEn: 'v', valueRu: 'м' },
-  { id: 'KeyB', valueEn: 'b', valueRu: 'и' },
-  { id: 'KeyN', valueEn: 'n', valueRu: 'т' },
-  { id: 'KeyM', valueEn: 'm', valueRu: 'ь' },
-  { id: 'Comma', valueEn: ',', valueRu: 'б' },
-  { id: 'Period', valueEn: '.', valueRu: 'ю' },
-  { id: 'Slash', valueEn: '/', valueRu: '.' },
-  { id: 'ArrowUp', valueEn: '▲', valueRu: '▲' },
-  { id: 'ShiftRight', valueEn: 'Shift', valueRu: 'Shift' },
-  { id: 'ControlLeft', valueEn: 'Ctrl', valueRu: 'Ctrl' },
-  { id: 'MetaLeft', valueEn: '⊞', valueRu: '⊞' },
-  { id: 'AltLeft', valueEn: 'Alt', valueRu: 'Alt' },
-  { id: 'Space', valueEn: 'RSS Virtual Keyboard', valueRu: 'Виртуальная клавиатура' },
-  { id: 'Delete', valueEn: 'Del', valueRu: 'Del' },
-  { id: 'AltRight', valueEn: 'Alt', valueRu: 'Alt' },
-  { id: 'ArrowLeft', valueEn: '◄', valueRu: '◄' },
-  { id: 'ArrowDown', valueEn: '▼', valueRu: '▼' },
-  { id: 'Arrowup', valueEn: '►', valueRu: '►' },
-  { id: 'ControlRight', valueEn: 'Ctrl', valueRu: 'Ctrl' },
+  {
+    id: 'Backquote', valueEn: '`', valueRu: 'ё', classEn: 'punctuation', classRu: 'letter', shiftValueEn: '~', shiftValueRu: '',
+  },
+  {
+    id: 'Digit1', valueEn: '1', valueRu: '1', classEn: 'digit', classRu: 'digit', shiftValueEn: '!', shiftValueRu: '!',
+  },
+  {
+    id: 'Digit2', valueEn: '2', valueRu: '2', classEn: 'digit', classRu: 'digit', shiftValueEn: '@', shiftValueRu: '"',
+  },
+  {
+    id: 'Digit3', valueEn: '3', valueRu: '3', classEn: 'digit', classRu: 'digit', shiftValueEn: '#', shiftValueRu: '№',
+  },
+  {
+    id: 'Digit4', valueEn: '4', valueRu: '4', classEn: 'digit', classRu: 'digit', shiftValueEn: '$', shiftValueRu: ';',
+  },
+  {
+    id: 'Digit5', valueEn: '5', valueRu: '5', classEn: 'digit', classRu: 'digit', shiftValueEn: '%', shiftValueRu: '%',
+  },
+  {
+    id: 'Digit6', valueEn: '6', valueRu: '6', classEn: 'digit', classRu: 'digit', shiftValueEn: '^', shiftValueRu: ':',
+  },
+  {
+    id: 'Digit7', valueEn: '7', valueRu: '7', classEn: 'digit', classRu: 'digit', shiftValueEn: '&', shiftValueRu: '?',
+  },
+  {
+    id: 'Digit8', valueEn: '8', valueRu: '8', classEn: 'digit', classRu: 'digit', shiftValueEn: '*', shiftValueRu: '*',
+  },
+  {
+    id: 'Digit9', valueEn: '9', valueRu: '9', classEn: 'digit', classRu: 'digit', shiftValueEn: '(', shiftValueRu: '(',
+  },
+  {
+    id: 'Digit0', valueEn: '0', valueRu: '0', classEn: 'digit', classRu: 'digit', shiftValueEn: ')', shiftValueRu: ')',
+  },
+  {
+    id: 'Minus', valueEn: '-', valueRu: '-', classEn: 'digit', classRu: 'digit', shiftValueEn: '_', shiftValueRu: '_',
+  },
+  {
+    id: 'Equal', valueEn: '=', valueRu: '=', classEn: 'digit', classRu: 'digit', shiftValueEn: '+', shiftValueRu: '+',
+  },
+  {
+    id: 'Backspace', valueEn: 'BackSpace', valueRu: 'BackSpace', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'Tab', valueEn: 'Tab', valueRu: 'Tab', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyQ', valueEn: 'q', valueRu: 'й', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyW', valueEn: 'w', valueRu: 'ц', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyE', valueEn: 'e', valueRu: 'у', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyR', valueEn: 'r', valueRu: 'к', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyT', valueEn: 't', valueRu: 'е', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyY', valueEn: 'y', valueRu: 'н', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyU', valueEn: 'u', valueRu: 'г', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyI', valueEn: 'i', valueRu: 'ш', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyO', valueEn: 'o', valueRu: 'щ', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyP', valueEn: 'p', valueRu: 'з', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'BracketLeft', valueEn: '[', valueRu: 'х', classEn: 'punctuation', classRu: 'letter', shiftValueEn: '{', shiftValueRu: '',
+  },
+  {
+    id: 'BracketRight', valueEn: ']', valueRu: 'ъ', classEn: 'punctuation', classRu: 'letter', shiftValueEn: '}', shiftValueRu: '',
+  },
+  {
+    id: 'Backslash', valueEn: '\\', valueRu: '\\', classEn: 'punctuation', classRu: 'punctuation', shiftValueEn: '|', shiftValueRu: '/',
+  },
+  {
+    id: 'CapsLock', valueEn: 'Caps', valueRu: 'Caps', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyA', valueEn: 'a', valueRu: 'ф', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyS', valueEn: 's', valueRu: 'ы', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyD', valueEn: 'd', valueRu: 'в', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyF', valueEn: 'f', valueRu: 'а', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyG', valueEn: 'g', valueRu: 'п', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyH', valueEn: 'h', valueRu: 'р', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyJ', valueEn: 'j', valueRu: 'о', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyK', valueEn: 'k', valueRu: 'л', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyL', valueEn: 'l', valueRu: 'д', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'Semicolon', valueEn: ';', valueRu: 'ж', classEn: 'punctuation', classRu: 'letter', shiftValueEn: ':', shiftValueRu: '',
+  },
+  {
+    id: 'Quote', valueEn: '\'', valueRu: 'э', classEn: 'punctuation', classRu: 'letter', shiftValueEn: '"', shiftValueRu: '',
+  },
+  {
+    id: 'Enter', valueEn: 'Enter', valueRu: 'Enter', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'ShiftLeft', valueEn: 'Shift', valueRu: 'Shift', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyZ', valueEn: 'z', valueRu: 'я', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyX', valueEn: 'x', valueRu: 'ч', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyC', valueEn: 'c', valueRu: 'с', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyV', valueEn: 'v', valueRu: 'м', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyB', valueEn: 'b', valueRu: 'и', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyN', valueEn: 'n', valueRu: 'т', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'KeyM', valueEn: 'm', valueRu: 'ь', classEn: 'letter', classRu: 'letter', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'Comma', valueEn: ',', valueRu: 'б', classEn: 'punctuation', classRu: 'letter', shiftValueEn: '<', shiftValueRu: '',
+  },
+  {
+    id: 'Period', valueEn: '.', valueRu: 'ю', classEn: 'punctuation', classRu: 'letter', shiftValueEn: '>', shiftValueRu: '',
+  },
+  {
+    id: 'Slash', valueEn: '/', valueRu: '.', classEn: 'punctuation', classRu: 'punctuation', shiftValueEn: '?', shiftValueRu: ',',
+  },
+  {
+    id: 'ArrowUp', valueEn: '▲', valueRu: '▲', classEn: 'digit', classRu: 'digit', shiftValueEn: '▲', shiftValueRu: '▲',
+  },
+  {
+    id: 'ShiftRight', valueEn: 'Shift', valueRu: 'Shift', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'ControlLeft', valueEn: 'Ctrl', valueRu: 'Ctrl', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'MetaLeft', valueEn: '⊞', valueRu: '⊞', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'AltLeft', valueEn: 'Alt', valueRu: 'Alt', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'Space', valueEn: 'RSS Virtual Keyboard', valueRu: 'Виртуальная клавиатура', classEn: 'digit', classRu: 'digit', shiftValueEn: 'RSS Virtual Keyboard', shiftValueRu: 'Виртуальная клавиатура',
+  },
+  {
+    id: 'Delete', valueEn: 'Del', valueRu: 'Del', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'AltRight', valueEn: 'Alt', valueRu: 'Alt', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
+  {
+    id: 'ArrowLeft', valueEn: '◄', valueRu: '◄', classEn: 'digit', classRu: 'digit', shiftValueEn: '◄', shiftValueRu: '◄',
+  },
+  {
+    id: 'ArrowDown', valueEn: '▼', valueRu: '▼', classEn: 'digit', classRu: 'digit', shiftValueEn: '▼', shiftValueRu: '▼',
+  },
+  {
+    id: 'Arrowup', valueEn: '►', valueRu: '►', classEn: 'digit', classRu: 'digit', shiftValueEn: '►', shiftValueRu: '►',
+  },
+  {
+    id: 'ControlRight', valueEn: 'Ctrl', valueRu: 'Ctrl', classEn: 'func', classRu: 'func', shiftValueEn: '', shiftValueRu: '',
+  },
 ];
 
 let lang = 'En';
 // const str = '';
-const key = document.querySelectorAll('.keyboard__key');
+let key = document.querySelectorAll('.keyboard__key');
 // const input = document.querySelector('.text__textarea');
 
 function setKeys(language) {
@@ -169,11 +297,17 @@ function setKeys(language) {
     for (let i = 0; i < key.length; i += 1) {
       key[i].innerHTML = keysArr[i].valueEn;
       key[i].id = keysArr[i].id;
+      key[i].classList.remove(`${keysArr[i].classRu}`);
+      key[i].classList.add(`${keysArr[i].classEn}`);
+      key = document.querySelectorAll('.keyboard__key');
     }
   } else if (language === 'Ru') {
     for (let i = 0; i < key.length; i += 1) {
       key[i].innerHTML = keysArr[i].valueRu;
       key[i].id = keysArr[i].id;
+      key[i].classList.remove(`${keysArr[i].classEn}`);
+      key[i].classList.add(`${keysArr[i].classRu}`);
+      key = document.querySelectorAll('.keyboard__key');
     }
   }
 }
@@ -183,11 +317,77 @@ setKeys(lang);
 function changeLanguage(language) {
   if (language === 'Ru') {
     lang = 'En';
-    setKeys(language);
+    setKeys(lang);
   } else {
     lang = 'Ru';
-    setKeys(language);
+    setKeys(lang);
   }
+}
+
+// Add Caps & Shift
+
+const isCaps = false;
+const isShift = false;
+
+function toUpperCase() {
+  key.forEach((el) => {
+    if (el.className.includes('letter') === true) {
+      el.classList.add('toUpperCase');
+    }
+  });
+}
+
+function toLowerCase() {
+  key.forEach((el) => {
+    if (el.className.includes('letter') === true) {
+      el.classList.remove('toUpperCase');
+    }
+  });
+}
+
+toUpperCase();
+toLowerCase();
+
+function addShiftValues() {
+  for (let i = 0; i < key.length; i += 1) {
+    if (lang === 'En') {
+      if (key[i].className.includes('punctuation') === true) {
+        key[i].innerHTML = keysArr[i].shiftValueEn;
+      }
+      if (key[i].className.includes('digit') === true) {
+        key[i].innerHTML = keysArr[i].shiftValueEn;
+      }
+    } else if (lang === 'Ru') {
+      if (key[i].className.includes('punctuation' || 'digit') === true) {
+        key[i].innerHTML = keysArr[i].shiftValueRu;
+      }
+      if (key[i].className.includes('digit') === true) {
+        key[i].innerHTML = keysArr[i].shiftValueRu;
+      }
+    }
+  }
+  toUpperCase();
+}
+
+function removeShiftValues() {
+  for (let i = 0; i < key.length; i += 1) {
+    if (lang === 'En') {
+      if (key[i].className.includes('punctuation') === true) {
+        key[i].innerHTML = keysArr[i].valueEn;
+      }
+      if (key[i].className.includes('digit') === true) {
+        key[i].innerHTML = keysArr[i].valueEn;
+      }
+    } else if (lang === 'Ru') {
+      if (key[i].className.includes('punctuation' || 'digit') === true) {
+        key[i].innerHTML = keysArr[i].valueRu;
+      }
+      if (key[i].className.includes('digit') === true) {
+        key[i].innerHTML = keysArr[i].valueRu;
+      }
+    }
+  }
+  toLowerCase();
 }
 
 // Temp for checking
@@ -195,4 +395,9 @@ function changeLanguage(language) {
 const cnglng = document.querySelector('.header');
 cnglng.addEventListener('click', () => {
   changeLanguage(lang);
+});
+
+const chkCaps = document.querySelector('.keyboard__description');
+chkCaps.addEventListener('click', () => {
+  addShiftValues();
 });
