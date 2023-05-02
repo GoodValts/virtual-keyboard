9175b2fef0b8e88f62b03a29a98640739927d10f
Martynjuk Ivan <martiniukgomel@gmail.com>
2023-05-02 11:05:59 +0300

    fix: add Eslint files  2 май 2023 г. 11:05:59

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  ignorePatterns: ['script2.js', 'script3.js'],
+  rules: {
+  },
+};
